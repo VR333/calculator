3784f36f34e60cd6351c6b999eb438a37a216f68
Rostyslav Vakh <rvakh@softserveinc.com>
2018-01-23 16:45:53 +0200

    improve webpack watch and npm run calc command

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path');
 module.exports = {
-  entry: './app/app.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build')
-  }
+    entry: './app/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build')
+    },
+    watch: true
 };
